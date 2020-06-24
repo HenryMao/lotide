d3725c35c46f9c0cdca5c87389f9916fbb12d69f
HenryMao <henry60308@live.cn>
2020-06-24 21:43:43 +0000

    countLetters added

---
+++ countLetters.js
@@ -0,0 +1,24 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion passed: ${actual} === ${expected}`);
+    return;
+  } else {
+    console.log(`Assertion failed ${actual} !== ${expected}`);
+    return;
+  }
+};
+
+
+const countLetters = function(str){
+  let result = {};
+  for(let char of str){
+    if(result[char]){
+      result[char]++;
+    }else{
+      result[char] = 1;
+    }
+  }
+  return result;
+}
+
+console.log(countLetters("aaabbbsssscccc"));
